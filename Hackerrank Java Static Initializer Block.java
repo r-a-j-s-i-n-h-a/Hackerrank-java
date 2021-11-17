39bee610e652b973b49465f6617e7a3ad29c8e73
r-a-j-s-i-n-h-a <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2021-11-17 19:51:14 +0530

    Java Static Initializer Block

---
+++ Hackerrank Java Static Initializer Block.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    static{
+     Scanner sc= new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+        if(a>0 && b>0){
+            System.out.println(a*b);
+        }
+        else{
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
