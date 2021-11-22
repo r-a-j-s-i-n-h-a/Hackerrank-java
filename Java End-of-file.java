1dcb71c3f616c097f5f9082f6f13734cbb604c5e
r-a-j-s-i-n-h-a <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2021-11-22 23:20:25 +0530

    Hackerrank problem

---
+++ Java End-of-file.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner line = new Scanner(System.in);
+        int counter = 1;
+        
+        while (line.hasNextLine()) {
+            String l = line.nextLine();
+
+            System.out.println(counter + " " + l);
+            counter++;
+        }
+    }
+}
