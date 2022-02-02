d8e0784f9e525dd3bcd1fce2ec5edf3788d3d8c7
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2022-02-02 23:53:45 +0530

    Create sort a string.java

---
+++ sort a string.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine().trim());
+        while(t-- > 0){
+            String s = sc.nextLine().trim();
+            Solution obj = new Solution();
+            System.out.println(obj.sort(s));
+        }
+    }
+}
+
+class Solution 
+{ 
+    String sort(String s) 
+    {
+        // code here
+        char ch[] = s.toCharArray();
+        Arrays.sort(ch);
+        String S = new String(ch);
+        return S;
+    }
+} 
